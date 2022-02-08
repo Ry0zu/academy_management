60364a0a9a9151b97c6237d837eaa9080e10f493
Ry0zu <ryozu38@gmail.com>
2022-02-08 02:44:50 +0100

    modal warning

--- listado_cursos.php
+++ listado_cursos.php
@@ -32,17 +32,17 @@ include("php/head.php");
                                 <!-- Modal -->
                         <div class=" modal fade" id="modelId<?php echo $row['id_curso'];?>" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
                             <div class="modal-dialog" role="document">
-                                <div class="modal-content">
+                                <div class="modal-content delete_modal">
                                     <div class="modal-header">
-                                        <h5 class="modal-title">Advertencia</h5>
+                                        <h5 class="modal-title"><img class="modal_image" src="img/warning.png">  Advertencia </h5>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
-                                    <div class="modal-body delete_modal">
-                                       ¿Estás seguro de que quieres borrar el curso <?php echo $row["nombre_curso"];?>?
+                                    <div class="modal-body">
+                                       ¿Está seguro de que desea eliminar el curso <span><?php echo $row["nombre_curso"];?></span>?
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cancelar</button>
-                                        <button onclick= "borrar_curso(<?php echo $row['id_curso'];?>)" type="button" class="btn btn-danger">Sí, borrar</button>
+                                        <button onclick= "borrar_curso(<?php echo $row['id_curso'];?>)" type="button" class="btn btn-danger">Eliminar</button>
                                     </div>
                                 </div>
                             </div>
